d0d353f566b51796f7f0d51dca1d2818f9bafdb9
Agne Rauluseviciute <agner0513@gmail.com>
2025-10-06 19:03:07 +0300

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
-      <h1 className="text-4xl font-bold text-white">CV DAY 1 :3</h1>
+      <h1 className="text-4xl font-bold text-white">TAI i LUPA AR i AKI AR i ANTAKI AUSKARA</h1>
     </main>
   );
 }
