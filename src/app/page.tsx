f390664a8892ccfc60da90962d8d82b9e5a69e00
Agne Rauluseviciute <agner0513@gmail.com>
2025-10-06 18:55:29 +0300

    update i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
-      <h1 className="text-4xl font-bold text-white">TAI I LUPA AR KUR AUSKARA OMG??</h1>
+      <h1 className="text-4xl font-bold text-white">TAI I LUPA AR i KUR AUSKARA OMG??</h1>
     </main>
   );
 }
