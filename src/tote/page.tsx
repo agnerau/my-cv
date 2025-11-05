d66a595578daad0b8c8e1cddbcd84e91f6cf2f0a
Agne Rauluseviciute <agner0513@gmail.com>
2025-11-05 17:30:32 +0200

    spin totebag

--- src/tote/page.tsx
+++ src/tote/page.tsx
@@ -1,22 +1,30 @@
 "use client";
-import React from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 
 const ToteBagModel: React.FC = () => {
   const { scene } = useGLTF("/tote/totebag.glb");
-  const texture = useLoader(THREE.TextureLoader, "/tote/lolipop.png");
-  <mesh position={[0, 0.4, 0.15]} scale={[0.6, 0.6, 0.6]}>
-    <planeGeometry args={[0.8, 0.8]} />
-    <meshBasicMaterial map={texture} transparent />
-  </mesh>;
-  return <primitive object={scene} scale={1.2} position={[0, -1.2, 0]} />;
+
+  const bagRef = useRef<THREE.Group>(null);
+
+  useFrame(() => {
+    if (bagRef.current) {
+      bagRef.current.rotation.y -= 0.003;
+    }
+  });
+
+  return (
+    <group ref={bagRef} scale={1.2} position={[0, -1.2, 0]}>
+      <primitive object={scene} />
+    </group>
+  );
 };
 
 const ToteBagScene: React.FC = () => {
   return (
-    <div className="w-full h-[50vh] bg-gradient-to-b from-gray-100 to-white">
+    <div className="w-full h-[70vh] bg-gradient-to-b from-gray-100 to-white">
       <Canvas
         shadows
         camera={{ position: [1, 1, 6], fov: 90 }}
